deee6deb01f930793824c4e9ed8543b80127047b
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2023-04-15 22:38:00 +0100

    change router to try to resolve mime type deployment error

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:anatdean/anatdean.github.io.git main
+# git push -f git@github.com:anatdean/anatdean.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:anatdean/faculty-vuejs.git main:gh-pages
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -6,7 +6,7 @@ const router = createRouter({
     // always scroll to top
     return { top: 0 }
   },
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
